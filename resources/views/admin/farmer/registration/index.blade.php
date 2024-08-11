23e8ee0c08ac77ad060f7c3e63cc5c1c7c81950e
uttamkumar9348 <uttamkumarmohanta73@gmail.com>
2024-08-12 00:21:40 +0530

    local

--- resources/views/admin/farmer/registration/index.blade.php
+++ resources/views/admin/farmer/registration/index.blade.php
@@ -109,7 +109,6 @@
                                         </td>
                                     </tr>
                                 @endforeach
-
                             </tbody>
                         </table>
                     </div>
